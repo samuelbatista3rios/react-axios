781fbf7599f080d5240af9fc72ccd273db667b4d
samuelbatista3rios <samuelbatista3rios@gmail.com>
2022-12-06 20:14:54 -0300

    Consumindo uma API com AXIOS em um blog

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import "./index.css";
+
+//páginas
+
+import Home from "./routes/Home";
+import NewPost from "./routes/NewPost";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewPost />,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
